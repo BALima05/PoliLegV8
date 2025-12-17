aa46a98b0ca31817decc85e931ee8fdab315988e
BALima05 <bernardo88xlima@hotmail.com>
2025-12-17 18:52:30 -0300

    :wrench: FD

--- Parte3_Fluxo_de_Dados_e_Unidade_de_Controle/fluxoDados.vhd
+++ Parte3_Fluxo_de_Dados_e_Unidade_de_Controle/fluxoDados.vhd
@@ -41,7 +41,7 @@ architecture structure_fluxoDados of fluxoDados is
         generic (
         addressSize : natural := 7;
         dataSize    : natural := 8;
-        datFileName : string  := "memInstr_conteudo.dat"
+        datFileName : string  := "memInstrPolilegv8.dat"
         );
         port (
             addr : in bit_vector( addressSize-1 downto 0);
@@ -88,7 +88,7 @@ architecture structure_fluxoDados of fluxoDados is
             inData      : in bit_vector (dataISize-1 downto 0);
             inDataStart : in bit_vector (dataMaxPosition-1 downto 0); -- posicao do bit mais significativo do valor util na entrada (bit de sinal)
             inDataEnd   : in bit_vector (dataMaxPosition-1 downto 0); -- posicao do bit menos significativo do valor util na entrada
-            outData     : out bit_vector (dataOSize-1 downto 0); -- dado de saida com tamanho dataOSize e sinal estendido 
+            outData     : out bit_vector (dataOSize-1 downto 0) -- dado de saida com tamanho dataOSize e sinal estendido 
         );
     end component; 
 
@@ -108,7 +108,7 @@ architecture structure_fluxoDados of fluxoDados is
         generic (
             addressSize : natural := 7;
             dataSize    : natural := 8;
-            datFileName : string := "memDados_conteudo_inicial.dat"
+            datFileName : string := "memDadosInicialPolilegv8.dat"
         );
         port ( 
             clock  : in bit;
@@ -155,7 +155,7 @@ architecture structure_fluxoDados of fluxoDados is
     signal reg_data1 : bit_vector(63 downto 0);
     signal reg_data2 : bit_vector(63 downto 0);
     
-    signal sign_extend : bit_vector(63 downto 0);
+    signal s_sign_extend : bit_vector(63 downto 0);
 
     signal alu_inputB : bit_vector(63 downto 0);
     signal alu_result : bit_vector(63 downto 0);
@@ -170,13 +170,26 @@ architecture structure_fluxoDados of fluxoDados is
 
     signal branch_offset : bit_vector(63 downto 0);
 
+    -- Sinais auxiliares para o calculo de endereco de instrucao
+    signal pc_idx : unsigned(7 downto 0);
+    signal addr_instr_0, addr_instr_1, addr_instr_2, addr_instr_3 : bit_vector(7 downto 0);
+    signal instr_b0, instr_b1, instr_b2, instr_b3 : bit_vector(7 downto 0);
+
+    -- Sinais auxiliares para o calculo de endereco de dados
+    signal alu_idx : unsigned(7 downto 0);
+    signal addr_data_0, addr_data_1, addr_data_2, addr_data_3 : bit_vector(7 downto 0);
+    signal addr_data_4, addr_data_5, addr_data_6, addr_data_7 : bit_vector(7 downto 0);
+
+    -- Sinais de dados de leitura
+    signal data_d0, data_d1, data_d2, data_d3, data_d4, data_d5, data_d6, data_d7 : bit_vector(7 downto 0);
+
 begin
 
     c_four <= (2 => '1', others => '0');    -- Constante 4
     opcode <= s_instruction(31 downto 21);  -- Opcode
      
     U_PC: reg 
-        generic map (dataSize => 7);
+        generic map (dataSize => 7)
         port map (
             clock  => clock,
             reset  => reset,
@@ -185,8 +198,20 @@ begin
             q      => pc_reg_val
         );
     
-    pc_extend(6 downto 0) <= pc_reg_val;
-    pc_extend(63 downto 7) <= (others => '0');
+    pc_idx <= unsigned(pc_out(7 downto 0));
+    
+    addr_instr_0 <= bit_vector(pc_idx);
+    addr_instr_1 <= bit_vector(pc_idx + 1);
+    addr_instr_2 <= bit_vector(pc_idx + 2);
+    addr_instr_3 <= bit_vector(pc_idx + 3);
+
+    -- Instanciação usando os sinais pre-calculados
+    MEMI_B0: memoriaInstrucoes port map (addr => addr_instr_0, data => instr_b0);
+    MEMI_B1: memoriaInstrucoes port map (addr => addr_instr_1, data => instr_b1);
+    MEMI_B2: memoriaInstrucoes port map (addr => addr_instr_2, data => instr_b2);
+    MEMI_B3: memoriaInstrucoes port map (addr => addr_instr_3, data => instr_b3);
+
+    instruction <= instr_b0 & instr_b1 & instr_b2 & instr_b3;
 
     U_IMEM: memoriaInstrucoes
         generic map ( 
@@ -216,7 +241,7 @@ begin
             inData       => s_instruction,
             inDataStart  => extendMSB,
             inDataEnd    => extendLSB,
-            outData      => sign_extend
+            outData      => s_sign_extend
         );
 
     U_ULA : ula 
@@ -243,7 +268,7 @@ begin
         generic map (dataSize => 64)
         port map (
             in0  => reg_data2,
-            in1  => sign_extend,
+            in1  => s_sign_extend,
             sel  => aluSrc,
             dOut => alu_inputB
         );
@@ -280,8 +305,30 @@ begin
             data_i  => reg_data2(7 downto 0), 
             data_o  => data_mem_out8
         );
-    data_memory_out(7 downto 0) <= data_mem_out8;
-    data_memory_out(63 downto 8) <= (others => '0');
+    
+    alu_idx <= unsigned(alu_result(7 downto 0));
+
+    addr_data_0 <= bit_vector(alu_idx);
+    addr_data_1 <= bit_vector(alu_idx + 1);
+    addr_data_2 <= bit_vector(alu_idx + 2);
+    addr_data_3 <= bit_vector(alu_idx + 3);
+    addr_data_4 <= bit_vector(alu_idx + 4);
+    addr_data_5 <= bit_vector(alu_idx + 5);
+    addr_data_6 <= bit_vector(alu_idx + 6);
+    addr_data_7 <= bit_vector(alu_idx + 7);
+
+    -- Instanciação dos 8 bancos usando os sinais de endereço
+    MEMD_B0: memoriaDados port map (clock => clock, wr => memWrite, addr => addr_data_0, data_i => reg_data2(63 downto 56), data_o => data_d0);
+    MEMD_B1: memoriaDados port map (clock => clock, wr => memWrite, addr => addr_data_1, data_i => reg_data2(55 downto 48), data_o => data_d1);
+    MEMD_B2: memoriaDados port map (clock => clock, wr => memWrite, addr => addr_data_2, data_i => reg_data2(47 downto 40), data_o => data_d2);
+    MEMD_B3: memoriaDados port map (clock => clock, wr => memWrite, addr => addr_data_3, data_i => reg_data2(39 downto 32), data_o => data_d3);
+    MEMD_B4: memoriaDados port map (clock => clock, wr => memWrite, addr => addr_data_4, data_i => reg_data2(31 downto 24), data_o => data_d4);
+    MEMD_B5: memoriaDados port map (clock => clock, wr => memWrite, addr => addr_data_5, data_i => reg_data2(23 downto 16), data_o => data_d5);
+    MEMD_B6: memoriaDados port map (clock => clock, wr => memWrite, addr => addr_data_6, data_i => reg_data2(15 downto  8), data_o => data_d6);
+    MEMD_B7: memoriaDados port map (clock => clock, wr => memWrite, addr => addr_data_7, data_i => reg_data2( 7 downto  0), data_o => data_d7);
+
+    -- Reconstrói a palavra de 64 bits
+    data_memory_out <= data_d0 & data_d1 & data_d2 & data_d3 & data_d4 & data_d5 & data_d6 & data_d7;
 
     U_ADD_PC4: adder_n 
         generic map (dataSize => 64)
@@ -303,17 +350,13 @@ begin
     
     U_SHIFT : two_left_shifts
         port map (
-            input  => sign_extend,
+            input  => s_sign_extend,
             output => branch_offset
         );
     
-end architecture structure_fluxoDados; 
-
-    
+end architecture structure_fluxoDados;
     
     
 
 
-    
-    
 
