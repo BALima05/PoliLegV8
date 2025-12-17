4fe37772ba2d023ff702ecdfd695b5791bdcad8d
BALima05 <bernardo88xlima@hotmail.com>
2025-12-17 01:10:40 -0300

    :wrench: correção da emória de instrucoes

--- Parte1_Biblioteca_componentes_basicos/memoriaInstrucoes.vhd
+++ Parte1_Biblioteca_componentes_basicos/memoriaInstrucoes.vhd
@@ -19,21 +19,40 @@ architecture arch_memInstr of memoriaInstrucoes is
     type mem_type is array (0 to mem_depht-1) of bit_vector (dataSize-1 downto 0);
 
     impure function init_mem(file_name : in string) return mem_type is
-        file     f       : text open read_mode is file_name; 
-        variable l       : line;
-        variable tmp_bv  : bit_vector(dataSize-1 downto 0);
-        variable tmp_mem : mem_type; 
-		variable i 	 : natural := 0;
+        file     f          : text open read_mode is file_name; 
+        variable l          : line;
+        variable tmp_bv     : bit_vector(dataSize-1 downto 0); -- Vetor final (32 bits)
+        variable file_val   : bit_vector(7 downto 0);          -- O que realmente existe no ficheiro (8 bits)
+        variable tmp_mem    : mem_type; 
+        variable i          : natural := 0;
     
     begin 
+        -- Inicializa a memória com zeros para evitar lixo
+        for j in tmp_mem'range loop
+            tmp_mem(j) := (others => '0');
+        end loop;
+
         while not endfile(f) loop
             readline(f, l);
-            read(l, tmp_bv);
+            -- Lemos apenas 8 bits do ficheiro
+            read(l, file_val);
+            
+            -- Nota: (dataSize - 8 - 1) assume que dataSize é 32. 
+            -- Se for genérico, a lógica é: (others => '0') & file_val
+            if dataSize > 8 then
+                tmp_bv := (others => '0'); -- Limpa tudo primeiro
+                tmp_bv(7 downto 0) := file_val; -- Coloca os 8 bits na parte baixa (LSB)
+                -- OU, se preferir concatenar explicitamente:
+                -- tmp_bv := bit_vector'(X"000000") & file_val; 
+            else
+                tmp_bv := file_val;
+            end if;
+
             tmp_mem(i) := tmp_bv;
-	    i := i + 1;
+            i := i + 1;
         end loop; 
         return tmp_mem;
-    end; 
+    end function;
 
     constant mem_data : mem_type := init_mem(datFileName);
 
